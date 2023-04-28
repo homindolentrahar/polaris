e7b12a642cfd4da25c70e44cc6785d7e7d4a3271
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2023-04-28 22:05:25 +0700

    Initial commit

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PolarisApp extends StatelessWidget {
+  const PolarisApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Polaris"),
+        ),
+      ),
+    );
+  }
+}
